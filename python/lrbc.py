da4d70e73fb3100690b3574a3f3f89624530307d
Ömer Faruk Birgül <birgulo@itu.edu.tr>
2022-01-18 10:57:32 +0300

    update lrbc

--- python/lrbc.py
+++ python/lrbc.py
@@ -14,4 +14,9 @@ plaintext = []
 for i in range(0, 16):
     n = random.randint(0, 1)
     plaintext.append(n)
+
 print(plaintext)
+pt1 = plaintext[0:2] + plaintext[8:10]
+pt2 = plaintext[2:4] + plaintext[10:12]
+pt3 = plaintext[4:6] + plaintext[12:14]
+pt4 = plaintext[6:8] + plaintext[14:16]
